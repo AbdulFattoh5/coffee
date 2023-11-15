bc8e3bad441fc3d01d32d05246fc24294cd2bbb9
Abdulfattoh <abdulfattokh55@gmail.com>
2023-11-15 13:55:18 +0500

    accordion

---
+++ script.js
@@ -0,0 +1,16 @@
+// accordion
+const accordion = document.getElementsByClassName('accordion__box');
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+
+        const content = this.querySelector('.content');
+
+        if (this.classList.contains('active')) {
+            content.style.height = content.scrollHeight + 'px';
+        } else {
+            content.style.height = '0';
+        }
+    });
+}
